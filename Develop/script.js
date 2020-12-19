a5e1d8bd2f572da0f7e281edfde8dad2a48bb270
unknown <km_si@ymail.com>
2020-12-19 15:56:36 -0500

    Random lowercase, uppercase, numeric char and special character generators

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ I WANT to randomly generate a password that meets certain criteria
 SO THAT I can create a strong password that provides greater security */
 
 // Global variable declarations and other
-
+//
 
 /*Promts to confirm password criteria
 length of the password (8 characters to 128 characters inclusive)
@@ -29,3 +29,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Random lowercase, uppercase, numeric char and special character generators reference for the fromCharCode => https://net-comber.com/charset.html
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+function getRandomUpperrCase() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
+}
+function getRandomNumericChar() {
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+function getRandomSpecialChar() {
+  const specialChars = [" ", "!", "$", "%", "&", "'", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~" ];
+  return specialChars[Math.floor(Math.random()*specialChars.length)];
+}
+
+//Testing the generators
+console.log (getRandomLowerCase(),getRandomUpperrCase(), getRandomNumericChar(), getRandomSpecialChar());
+
+
+
